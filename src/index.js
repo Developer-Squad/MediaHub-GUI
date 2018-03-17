6ea0d2dad9a9f607ece2cbf7503853798653c835
Nithin Raj <nithin.raj@iiitb.org>
2018-03-17 23:19:46 +0530

    index.js updates

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.css';
 import { Home } from './container/Home';
-
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Home />,document.getElementById('root'));
-
+registerServiceWorker();
 
