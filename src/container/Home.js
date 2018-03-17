de038d9fa408da1a239c9e9ae405160155d48fcf
Nithin Raj <nithin.raj@iiitb.org>
2018-03-18 00:52:11 +0530

    home container updated

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Logo } from '../components/logo';
+import { Searchbar } from '../components/searchbar';
 
 export class Home extends React.Component {
     render() {
-        return <Logo />;
+    const myhome = (<div>
+                    <Logo />
+                    <Searchbar />
+                </div>
+                );
+    return (myhome);
     }
 }
