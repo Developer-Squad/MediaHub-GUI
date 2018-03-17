7b3e6b135232fe29b2a295e7a66416cc1f8facfe
Arvind R <r.arvind@iiitb.org>
2018-03-17 22:40:58 +0530

    Stage 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.css';
-import {Loader} from './components/loader.js'
-
-ReactDOM.render(<Loader />,document.getElementById('root'));
