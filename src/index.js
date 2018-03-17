2ce8301954a9c126c31bc634282d9d37f9f25748
Nithin Raj <nithin.raj@iiitb.org>
2018-03-17 22:59:01 +0530

    index.js updated

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './assets/index.css';
 import { Home } from './container/Home';
 
-<<<<<<< HEAD
-ReactDOM.render(<Home />,document.getElementById('root'));
-=======
 
->>>>>>> c8d41cbf6e68053495f2a37c074490c3ead8d621
+ReactDOM.render(<Home />,document.getElementById('root'));
+
+
