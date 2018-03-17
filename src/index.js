a103ef1e3e5c32e8d805cec876c17fac4f5d87a3
Arvind R <r.arvind@iiitb.org>
2018-03-17 20:45:46 +0530

    Changed file structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/index.css';
 
-ReactDOM.render(<h1>Media Hub</h1>,document.getElementById('root'));
+ReactDOM.render(<h1 className="title">Media Hub</h1>,document.getElementById('root'));
