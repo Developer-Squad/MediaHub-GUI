10690ccd4682ee117189363497e1377bdb92b6c0
Ronak Doshi <ronakvipul.doshi@iiitb.org>
2018-03-17 23:36:14 +0530

    logog

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.css';
 import { Home } from './container/Home';
+import {Logo} from './components/logo.js'
 
 
-ReactDOM.render(<Home />,document.getElementById('root'));
+ReactDOM.render(<Logo />,document.getElementById('root'));
 
 
