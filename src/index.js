5852e2c4379bce558a8272bd0c9c9f9f94103fef
Ronak Doshi <ronakvipul.doshi@iiitb.org>
2018-03-17 22:39:23 +0530

    logo

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.css';
 
-ReactDOM.render(<h1 className="title">Media Hub</h1>,document.getElementById('root'));
+
