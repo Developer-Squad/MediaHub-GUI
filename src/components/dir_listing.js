9812469c50e6c55b98842429fb068d1b73319d39
Nithin Raj <nithin.raj@iiitb.org>
2018-03-18 20:55:15 +0530

    Listing

--- src/components/dir_listing.js
+++ src/components/dir_listing.js
@@ -1,1 +1,35 @@
 import React from 'react';
+
+export class Row extends React.Component {
+    /*
+    constructor(props){
+        super(props);
+        this.state = {
+            value:null
+        };
+    }
+    val(){
+        var type = this.props.file.filetype;
+        if(type === 'mp4' || type === 'mkv' || type === 'avi')
+            this.setState({value:'../assets/video.png'});
+        else if(type === 'mp3' || type === 'wav')
+            this.setState({value:'../assets/music.png'});
+        else if(type === 'folder')
+            this.setState({value:'../assets/folder.png'});
+        else
+            this.setState({value:'../assets/icon.png'});
+    }
+    setsrc(e) {
+        this.val;
+        e.target.setAttribute('src', this.state.value);
+    }*/
+    render() {
+        return(
+        <div className = "row">
+            <img src = 'assets/folder.png' />
+            <h3>{this.props.file.filename}</h3>
+            <h3>{this.props.file.filesize}</h3>
+        </div>
+        );
+    }
+}
--- src/container/Home.js
+++ src/container/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Logo } from '../components/logo';
-import { Searchbar } from '../components/searchbar';
-
+/*import { Logo } from '../components/logo';
+import { Searchbar } from '../components/searchbar';*/
+import { Row } from '../components/dir_listing';
 export class Home extends React.Component {
     render() {
+    var file_obj = {filename: 'qw',
+                    filetype: 'mp3',
+                    filesize: '23'
+                };
     const myhome = (<div className="logos">
-                    <Logo />
-                    <Searchbar />
+                    <Row file = {file_obj}/>
                 </div>
                 );
     return (myhome);
