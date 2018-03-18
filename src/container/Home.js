7e756f3ad2b5a08fa50fa12ae0180c85a966ce75
Arvind R <r.arvind@iiitb.org>
2018-03-18 21:01:52 +0530

    Stage 2

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,18 +1,14 @@
 import React from 'react';
-/*import { Logo } from '../components/logo';
-import { Searchbar } from '../components/searchbar';*/
-import { Row } from '../components/dir_listing';
+import { Logo } from '../components/logo';
+import { Searchbar } from '../components/searchbar';
+
 export class Home extends React.Component {
-    /*
-    render() {
-    var file_obj = {filename: 'qw',
-                    filetype: 'mp3',
-                    filesize: '23'
-                };
-    const myhome = (<div className="logos">
-                    <Row file = {file_obj}/>
-                </div>
-                );
-    return (myhome);
-    }*/
+    render(){
+        return (
+            <div className="logos">
+            <Logo />
+            <Searchbar />
+            </div>
+        );
+    }
 }
