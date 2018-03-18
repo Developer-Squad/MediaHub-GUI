29e5aed6c217fcd413736ec88f5fc531c0e1fb1d
Nithin Raj <nithin.raj@iiitb.org>
2018-03-19 00:14:11 +0530

    dir_list updated

--- src/components/dir_listing.js
+++ src/components/dir_listing.js
@@ -1,33 +1,22 @@
 import React from 'react';
 
 export class Row extends React.Component {
-    /*
-    constructor(props){
-        super(props);
-        this.state = {
-            value:null
-        };
-    }
-    val(){
-        var type = this.props.file.filetype;
-        if(type === 'mp4' || type === 'mkv' || type === 'avi')
-            this.setState({value:'../assets/video.png'});
-        else if(type === 'mp3' || type === 'wav')
-            this.setState({value:'../assets/music.png'});
-        else if(type === 'folder')
-            this.setState({value:'../assets/folder.png'});
-        else
-            this.setState({value:'../assets/icon.png'});
-    }
-    setsrc(e) {
-        this.val;
-        e.target.setAttribute('src', this.state.value);
-    }*/
     render() {
+        const type = this.props.file.filetype;
+        if(type === 'mp4' || type === 'mkv' || type === 'avi')
+            var tag = <td><img src = {require('../assets/video.png')} className="image-dir" /></td>;
+        else if(type === 'mp3' || type === 'wav')
+            var tag = <td><img src={require('../assets/music.png')} className="image-dir" /></td>;
+        else if(type === 'folder')
+            var tag = <td><img src={require('../assets/folder.png')} className="image-dir" /></td>;
+        else
+            var tag = <td><img src={require('../assets/icon.png')} className="image-dir" /></td>;
+        //const tag = <img src = {require('../assets/icon.png')} className="image-dir" />
         return(
         <table className = "row">
         	<tr>
-	            <td><img src = {require('../assets/folder.png')} className="image-dir" /></td>
+                {/*<td><img src = {require(src_val)} className="image-dir" /></td>*/}
+                <td>{tag}</td>
 	            <td><h3 className="filename-dir">{this.props.file.filename}</h3></td>
 	            <td><h3 className="filesize-dir">{this.props.file.filesize}</h3></td>
 	        </tr>
