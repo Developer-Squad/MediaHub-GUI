43ae58f66e3b8093380101e597bec93ab0312ff6
Arvind R <r.arvind@iiitb.org>
2018-03-20 22:18:45 +0530

    redirect

--- src/index.js
+++ src/index.js
@@ -2,9 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.css';
 import { Home } from './container/Home';
+import {Results} from './container/searchresult'
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<Home />,document.getElementById('root'));
+
+class Display extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {display: '/'};
+        this.handleClick = this.handleClick.bind(this);
+        this.component = this.component.bind(this);
+    }
+    component(){
+        if(this.state.display == '/'){
+            return (<Results />);
+        }
+        else if(this.state.display == '/home'){
+            return (<Home />);
+        }
+    }
+
+    handleClick(){
+        if(this.state.display == '/home'){
+            this.setState({display:'/'});
+        }
+        else{
+            this.setState({display:'/home'});
+        }
+        return this.component();
+    }
+    render(){
+        return (
+            <div className="aaa">
+            <button className="sss" onClick= {this.handleClick}>Change</button>
+            {this.component.call()}
+            </div>
+        );
+    }
+
+
+}
+
+ReactDOM.render(<Display />,document.getElementById('root'));
 registerServiceWorker();
 
 
