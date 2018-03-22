ddc7eac89e132faa12bc827ff7e3c4ee23d13623
Nithin Raj <nithin.raj@iiitb.org>
2018-03-22 16:18:13 +0530

    button redirect added

--- src/components/button.js
+++ src/components/button.js
@@ -1,11 +1,19 @@
 import React from 'react';
 //import { Loader } from './loader';
 import { Link } from 'react-router-dom';
+import { Loader } from './loader';
 export class Button extends React.Component {
+   /* constructor(props) {
+        super(props);
+        this.state={
+            location:"home"
+        }
+        this.handleClick=this.handleClick.bind(this);
+    }*/
     render(){
         return (
-                <Link to="/searchresults">
-                    <button className="searchbutton" ></button>
+                <Link to="searchresults">
+                    <button className="searchbutton"></button>
                 </Link>
         );
     }
--- src/components/row.js
+++ src/components/row.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Table from './dir_list'
-
+//import Table from './dir_list'
+import { Table } from 'reactbulma'; 
 export class Row extends React.Component {
     render() {
         const type = this.props.file.filetype;
@@ -14,12 +14,25 @@ export class Row extends React.Component {
             var tag = <td><img src={require('../assets/icon.png')} className="image-dir" /></td>;
         //const tag = <img src = {require('../assets/icon.png')} className="image-dir" />
         return(
+<div className="aaa">
+            <Table bordered>
+  <Table.Body>
+    <Table.Tr>
+      <Table.Td>{tag}</Table.Td>
+      <Table.Td>{this.props.file.filename}</Table.Td>
+      <Table.Td>{this.props.file.filesize}</Table.Td>
+    </Table.Tr>
+  </Table.Body>
+</Table>
+</div>
+
+/*
         	<tr class="row">
-                {/*<td><img src = {require(src_val)} className="image-dir" /></td>*/}
+                {/*<td><img src = {require(src_val)} className="image-dir" /></td>}
                 <td>{tag}</td>
 	            <td><h3 className="filename-dir">{this.props.file.filename}</h3></td>
 	            <td><h3 className="filesize-dir">{this.props.file.filesize}</h3></td>
-	        </tr>
+	        </tr>*/
         );
     }
 }
