54554a2c09e1b200f46641b12e3a5beaa088ad32
Ronak Doshi <ronakvipul.doshi@iiitb.org>
2018-03-23 18:44:35 +0530

    bulma searchbar and list added

--- src/container/searchresult.js
+++ src/container/searchresult.js
@@ -7,15 +7,20 @@ import {Dir_list} from '../components/dir_list';
 export class Results extends React.Component {
     render(){
         var file_des = {
-            filename:"qwerty",
+            filename:"Rabin Karp algorithm",
             filesize:"15MB",
-            filetype:"folder"
+            filetype:"mp4"
+        };
+        var file_des1 = {
+            filename:"Narcos Season 2",
+            filesize:"15MB",
+            filetype:"mp4"
         };
         return (
             <div className='aaa'>
             <Navbar search={<Home />}/>
             <br /><br /><br />
-            <Dir_list />
+            <Dir_list file={file_des1}/>
             </div>
         );
     }
